34939f62bd44144b0f172925374988ed43fad6a4
Abhishek Srivastava <abhishek8developer@gmail.com>
2022-07-11 00:55:29 +0530

    Solutions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ed5d1479d05433e"; }
+/******/ 		__webpack_require__.h = function() { return "94abab7f28f7866d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Header from './header'
 import Footer from './footer'
 
-export default function About(){
+export default function About() {
     return (
         <div id="aboutus">
             <Header />
@@ -12,7 +12,7 @@ export default function About(){
                 <div className="header-right-image wow zoomIn" style={{ display: 'inline-block', verticalAlign: 'middle'}}>
                     <img src="/images/ezgif.com-gif-maker.gif" alt="header right image" />
                 </div> */}
-                <div className="container">        
+                <div className="container">
                     <div className="col-lg-12">
                         <div className="heading-content">
                             <h1 className="title wow fadeInDown">ABOUT US</h1>
@@ -26,17 +26,17 @@ export default function About(){
                     <div className="row content">
                         <header className="section-header text-center justify-content-center wow fadeInDown">
                             <h1 className='blue-underline-heading'>Our Mission</h1>
-                            <p>As a Cloud Oriented company, Bootlabs is simplifying cloud, with our Frameworks, Strategies and Consulting. Our focus is to make our Clients achieve reliability , availability and excellent reduction in time to market their products. 
+                            <p>As a Cloud Oriented company, Bootlabs is simplifying cloud, with our Frameworks, Strategies and Consulting. Our focus is to make our Clients achieve reliability , availability and excellent reduction in time to market their products.
                                 We are new-gen and new-tech company in pursuit to make Multi-cloud way of life for everyone.
                             </p>
                             <p>
-                            We are a Boutique Tech Consulting partner, specializing on Cloud Native Solutions. We are obsessed about anything “CLOUD”.
-                            Our goal is to seamlessly automate development lifecycle,  modernize infrastructure and its associated applications.
-                            With a product mind-set, we enable start-ups and enterprises on cloud transformation, cloud migration, end to end automation and managed cloud services. We are eager to research, discover, automate, adapt, empower and deliver quality solutions on-time every-time. We are passionate about customer success. With the right blend of experience and exuberant youth in our in-house team, we have made significant impact to customers.
+                                We are a Boutique Tech Consulting partner, specializing on Cloud Native Solutions. We are obsessed about anything “CLOUD”.
+                                Our goal is to seamlessly automate development lifecycle,  modernize infrastructure and its associated applications.
+                                With a product mind-set, we enable start-ups and enterprises on cloud transformation, cloud migration, end to end automation and managed cloud services. We are eager to research, discover, automate, adapt, empower and deliver quality solutions on-time every-time. We are passionate about customer success. With the right blend of experience and exuberant youth in our in-house team, we have made significant impact to customers.
                             </p>
                         </header>
                     </div>
-                        <a href="#approach" className="boxed-btn btn-rounded">Join Us</a> 
+                    <a href="#approach" className="boxed-btn btn-rounded">Join Us</a>
                 </div>
             </section>
             <section className='team'>
@@ -59,6 +59,30 @@ export default function About(){
                             </div>
                         </div>
                         <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="200">
+
+                            <div className="img-fluid male-4" />
+                            <h5>Mayank Awasthi</h5>
+                            <p>Branding & Marketing</p>
+                            <div className="social justify-content-center">
+                                <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
+                                <a href=""><i className="ri-twitter-fill"></i></a>
+                                <a href=""><i className="ri-facebook-fill"></i></a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="300">
+                            <div className="img-fluid male-3" />
+                            <h5>Ravi R.</h5>
+                            <p>VP Consulting</p>
+                            <div className="social justify-content-center">
+                                <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
+                                <a href=""><i className="ri-twitter-fill"></i></a>
+                                <a href=""><i className="ri-facebook-fill"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-2" />
+                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="400">
                             <div className="img-fluid male-2" />
                             <h5>Karthick Dharman</h5>
                             <p>Cloud Architect</p>
@@ -68,8 +92,8 @@ export default function About(){
                                 <a href=""><i className="ri-facebook-fill"></i></a>
                             </div>
                         </div>
-                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="300">
-                        <div className="img-fluid male-3" />
+                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="100">
+                            <div className="img-fluid male-5" />
                             <h5>Aravind kumar</h5>
                             <p>Development Lead</p>
                             <div className="social justify-content-center">
@@ -77,31 +101,9 @@ export default function About(){
                                 <a href=""><i className="ri-twitter-fill"></i></a>
                                 <a href=""><i className="ri-facebook-fill"></i></a>
                             </div>
+
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-2"/>
-                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="400">
-                            <div className="img-fluid male-4" />
-                            <h5>Mayank Awasthi</h5>
-                            <p>Branding & Marketing</p>
-                            <div className="social justify-content-center">
-                            <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
-                            <a href=""><i className="ri-twitter-fill"></i></a>
-                            <a href=""><i className="ri-facebook-fill"></i></a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="100">
-                            <div className="img-fluid male-5" />
-                            <h5>Ravi R.</h5>
-                            <p>VP Consulting</p>
-                            <div className="social justify-content-center">
-                                <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
-                                <a href=""><i className="ri-twitter-fill"></i></a>
-                                <a href=""><i className="ri-facebook-fill"></i></a>
-                            </div>
-                        </div>
-                        <div className="col-lg-1"/>
+                        <div className="col-lg-1" />
                     </div>
                     <div className="row">
                         <div className="col-lg-12 team-box aos-init aos-animate wow fadeInDown" data-aos="fade-up" data-aos-delay="400">
@@ -109,9 +111,9 @@ export default function About(){
                             <h5>Radhika</h5>
                             <p>CHRO</p>
                             <div className="social justify-content-center">
-                            <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
-                            <a href=""><i className="ri-twitter-fill"></i></a>
-                            <a href=""><i className="ri-facebook-fill"></i></a>
+                                <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
+                                <a href=""><i className="ri-twitter-fill"></i></a>
+                                <a href=""><i className="ri-facebook-fill"></i></a>
                             </div>
                         </div>
                     </div>
