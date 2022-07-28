1a285d7fc904beaecd0c793a96865a9236b2e411
unknown <swatigandhi43@gmail.com>
2022-07-28 23:13:50 +0530

    career page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a459c43c755f7301"; }
+/******/ 		__webpack_require__.h = function() { return "38c8637fc7605c02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/career.js
+++ pages/career.js
@@ -1,29 +1,76 @@
-import Header from './header'
-import Footer from './footer'
+import Header from "./header";
+import Footer from "./footer";
 
 export default function Career() {
-    return (
-        <>
-            <Header />
-            <section class="how-it-work-area" id="approach">
-                <div class="shape-1"><img src="/images/shape/08.png" alt="" /></div>
-                <div class="shape-2"><img src="/images/shape/09.png" alt="" /></div>
-                <div class="shape-3"><img src="/images/shape/08.png" alt="" /></div>
-                <div class="shape-4"><img src="/images/shape/09.png" alt="" /></div>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-10">
-                            <div class="section-title">
-                                <h3 class="title">SUBSCRIBE US</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" class="btn btn-primary">
-                        <a href="RegistrationForm.html">Click Here</a>
-                    </button>
+  const jobs = [
+    {
+      title: "Senior Frontend Developer - WordPress",
+      description:
+        "Job Overvirew: Bootlabs is looking for an ambitious and self-driven Senior Front-end Developer who is motivated to combine the art of design with the art...",
+      city: "Bangalore",
+    },
+    {
+      title: "Associate Software Engineer - ReactJS",
+      description:
+        "Job Overvirew: We are looking for some fresh software developer to train and develop react development skills in-house which is hard to find in a",
+      city: "Bangalore",
+    },
+    {
+      title: "Sr. Digital Content writer",
+      description:
+        "Job Overvirew: We are looking for candidates who posses strong writing, editing and competitive research skills to join as an SEO content writer.",
+      city: "Bangalore",
+    },
+    {
+      title: "Social Media Marketing Manager",
+      description:
+        "Job Overvirew: The Social Media Marketing Manager is both a creative and strategic role. You'll help shape our social strategy, bringing the Bootlabs brand",
+      city: "Bangalore",
+    },
+  ];
+  return (
+    <>
+      <Header />
+      <section class="how-it-work-area" id="career">
+        <div class="container-fluid">
+          <div class="row justify-content-center">
+            <div className="col-lg-12 bg-image">
+              <h2> Join Our Team </h2>
+              <p>
+                {" "}
+                We're simplifying the cloud for developers. We need talented
+                people like you to help us support and expand our
+                high-performance cloud infrastructure.
+              </p>
+            </div>
+          </div>
+
+          <div className="row bg-grey">
+            <div className="container" data-aos="fade-up">
+              <div className="row">
+                <div className="col-lg-12 text-center ">
+                  <h1 className="blue-underline-heading">Current Opening</h1>
                 </div>
-            </section>
-            <Footer />
-        </>
-    )
+              </div>
+              {jobs.map((job) => (
+                <div className="row box">
+                  <div className="col-lg-9">
+                    <label>{job.title}</label>
+                    <p>
+                      {job.description}
+                    </p>
+                    <a className="apply">APPLY NOW</a>
+                  </div>
+                  <div className="col-lg-3">
+                    <i class="fas fa-map-marker-alt"></i> {job.city}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
